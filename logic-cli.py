85710ea523f1289df11ba1cb3df04e76c28c97f1
Eric Hayter <hayter.eric@gmail.com>
2023-06-23 14:50:09 -0400

    working on parsing logical functions
    working on a recursive function to generate all possible values of T and F to build truth tables for logical functions

--- logic-cli.py
+++ logic-cli.py
@@ -3,39 +3,63 @@ import re
 
 class TruthValue(Enum):
     """Enumeration class for values in a truth table """
-    T = 1   # True
-    F = 0   # False
-    D = -1  # Don't care
+    T = True   # True
+    F = False   # False
+    D = None  # Don't care
+
+    def __repr__(self):
+        return self.name
 
 
 def parse_file(file_name: str) -> list[list[TruthValue]]:
-    with open(file_name) as file:
-        VARIABLES = list(file.readline().strip())
-        truth_table = [[None,None] for _ in range(len(VARIABLES) - 1)]
-        for line in file:
-            if line == '\n':
-                break
+    truth_table = dict()
+    # read in the function
+    # parse it into an actual python function to calculate the output
 
-            output = to_truth_value(line[-1])
-            # read in the non-ouput characters and use them as indexes
-            for chr in line[:-1]:
+    if False:
+        with open(file_name) as file:
+            truth_table['VARIABLES'] = list(file.readline().strip())
+            for line in file:
+                line = line.strip()
+                if line == '\n':
+                    break
+               
+                # still not error catching in it
+                output = to_truth_value(line[-1])
+                truth_values = tuple([to_truth_value(s) for s in line[:-1]])
+                truth_table[truth_values] = output
                 
+    return truth_table
 
-    return VARIABLES
+def input_combos(n: int, values:list[list[TruthValue]] = []) -> list[tuple(TruthValue)]:
+    if n <= 0:
+        return [tuple(c) for c in values]
+    else:
+        v = len(values)
+        values = values * 2
+        if v == 0:
+            v += 2
+            values += [[], []]
+        for i in range(v):
+            values[i].append(TruthValue.F)
+            values[i + v - 1].append(TruthValue.T)
+            return input_combos(n - 1, values)
+
 
 def to_truth_value(s: str) -> TruthValue:
-    if s == 'T' or '1':
+    if s == 'T' or s == '1':
         return TruthValue.T
-    elif s == 'F' or '0':
+    elif s == 'F' or s == '0':
         return TruthValue.F
     elif s == 'D':
         return TruthValue.D
     else:
-        yield Exception(f'invalid truth symbol {s}')
+        raise Exception(f'invalid truth symbol {s}')
 
 
 def main():
-    print(parse_file('or.txt'))
+    #print(parse_file('or.txt'))
+    print(input_combos(3))
     return
 
 if __name__ == '__main__':
