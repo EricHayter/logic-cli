b7d02e261c91f87449d8fd88fdee3f8f08063058
Eric Hayter <hayter.eric@gmail.com>
2023-07-07 20:10:07 -0400

    formatting

--- karnaugh.py
+++ karnaugh.py
@@ -1,12 +1,9 @@
 import math
 import itertools
 
+
 def simplify_function(func: dict) -> dict:
     num_vars = int(math.log(len(func), 2))
-    # check for paris of values/prime implicants
-    # check from biggest to smallest
-    # e.g. can it be simplified to True or False?
-    # then can it be simplified down to one variable?
     prime_implicants = []
     minterms = [c for c in func if func[c]]
     for implicant in get_implicants(num_vars):
@@ -14,7 +11,7 @@ def simplify_function(func: dict) -> dict:
             break
         included = included_minterms(func.keys(), implicant)
         for m in included:
-            if not func[m]: # implicant can't wont work if minterm is false
+            if not func[m]:  # implicant can't wont work if minterm is false
                 break
         else:
             prime_implicants.append(implicant)
@@ -37,11 +34,10 @@ def included_minterms(minterms: list[tuple], implicant: tuple) -> list[tuple]:
     return included
 
 
-# create a generator to make combos e.g.
-# (None, None, None, None) (True, None, None, None) ...
 def get_implicants(n: int) -> list:
-    imp = list(set(itertools.product([True, False, None], repeat = n)))
-    return sorted(imp, key = lambda l : l.count(None), reverse=True)
+    imp = list(set(itertools.product([True, False, None], repeat=n)))
+    return sorted(imp, key=lambda l: l.count(None), reverse=True)
+
 
 def input_combos(n: int, values: list[list[bool]] = [[]]) -> list[tuple]:
     if n <= 0:
@@ -49,4 +45,3 @@ def input_combos(n: int, values: list[list[bool]] = [[]]) -> list[tuple]:
     else:
         values = [e + [v] for e in values for v in (False, True)]
         return input_combos(n - 1, values)
-
